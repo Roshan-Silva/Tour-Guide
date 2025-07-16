34b9ce47256003f33c2254ed47d0d11395ea610d
Roshan-Silva <roshan0122silva@gmail.com>
2025-07-16 23:35:22 +0530

    Backend setup and ensure DB connection

---
+++ tour-guide-backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// initialize express app
+const app = express();
+
+// port to listen on
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('✅ Tour Guide API is working')
+})
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    })
+}).catch(err => console.error('MongoDB connection error:', err));
+
